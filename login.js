d9dff9197805c0d2db48edcdc8f0e01fb1ea5743
ErikaGillig <90275725+ErikaGillig@users.noreply.github.com>
2023-02-11 11:21:02 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,32 @@
+function login (){
+    let name = prompt(" Ingresa tu nombre");
+    let respuesta = "nuevo";
+    let status;
+    let nuevo = "yes";
+
+if (pregunta === respuesta){
+            porSi();
+          } else{
+status = prompt("Sos Nuevo usuario?");
+}
+
+function porSi(){
+        document.getElementsByClassName("button_par")[0];
+    }  
+
+if (name != respuesta && status === nuevo){
+        document.getElementsByClassName("button_par")[0].innerHTML = "Bienvenido/a " + question + ", porfavor cree una cuenta";
+    }else if(name === respuesta){
+        porSi(); 
+    }else{
+        document.getElementsByClassName("button_par")[0].innerHTML = "<span>Ocurrio un error intente poner su nombre nuevamente </span>";
+    } 
+
+}
+
+function logout(){
+
+    // window.location.reload(); only works with your environnement or website
+  document.getElementsByClassName("button_par")[0].innerHTML = "";
+}
+
